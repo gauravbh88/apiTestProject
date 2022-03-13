e7b9ee2c34f729f23606250ceacca3093f84f6ee
gauravbh88@gmail.com <Nexuss"12345>
2022-03-13 14:56:30 +0100

    initial commit

---
+++ src/test/java/com/testAssignmentDnb/resources/UtilityClass.java
@@ -0,0 +1,115 @@
+package com.testAssignmentDnb.resources;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Random;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+
+public class UtilityClass {
+
+
+
+	public static RequestSpecification requestSpec;
+	public static ResponseSpecification responseSpec;
+	
+	public static RequestSpecification requestSpecification() throws IOException
+	{
+
+		if(requestSpec==null)
+		{
+			
+			{
+				PrintStream log = new PrintStream(new FileOutputStream("logging.txt"));
+				requestSpec = new  RequestSpecBuilder()
+						.setContentType("application/json")
+						.setBaseUri(getGlobalValue("baseUrl")).addFilter(RequestLoggingFilter.logRequestTo(log))
+						 .addFilter(ResponseLoggingFilter.logResponseTo(log))
+						.build();
+				
+				
+				 return requestSpec;
+				 
+				 
+		
+			}
+			
+			
+			
+		}
+		return requestSpec;
+		
+		
+	}
+	
+	public static ResponseSpecification responseSpecification() throws IOException
+	{
+
+	
+		
+	
+		   responseSpec = new ResponseSpecBuilder()
+						  .expectStatusCode(200)
+						  .expectContentType(ContentType.JSON)
+						  .build();
+			
+		return responseSpec;
+		
+					
+		
+		
+		
+	}
+	
+	
+	public static HashMap<String, String> createBody() {
+		
+		Random random = new Random();
+		String name = "TestUser" + random.nextInt();
+		
+		byte[] array = new byte[7]; // length is bounded by 7
+	    new Random().nextBytes(array);
+	    String defaultpassword = new String(array, Charset.forName("UTF-8"));
+		HashMap<String,String> postContent = new HashMap<String, String>();
+		postContent.put("name",name);
+		postContent.put("email",name+"@gmail.com");
+		postContent.put("password",defaultpassword);
+		return postContent;
+	}
+	
+	
+	public static String getGlobalValue(String key) throws IOException
+	{
+		Properties prop =new Properties();
+		FileInputStream fis= new FileInputStream("src/main/resources/global.properties");
+		//FileInputStream fis =new FileInputStream("C:\\Users\\5012gbh\\eclipse-workspace\\testAssignmentDnb\\src\\test\\java\\resources\\global.properties");
+		prop.load(fis);
+		return prop.getProperty(key);
+	
+		
+		
+	}
+	
+	public String getJsonPath(Response response,String key)
+	{
+		  String resp=response.asString();
+		JsonPath   js = new JsonPath(resp);
+		return js.get(key).toString();
+	}
+	
+	
+}
---
+++ src/test/java/com/testAssignmentDnb/test/APITests.java
@@ -0,0 +1,134 @@
+package com.testAssignmentDnb.test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.util.HashMap;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.testAssignmentDnb.resources.UtilityClass;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+
+import io.restassured.response.Response;
+
+public class APITests extends UtilityClass {
+
+	HashMap<String, String> postContent;
+	String createUserName;
+	String createUserId;
+	String createUserEmail;
+	String password;
+	String loginVerificationToken;
+	String loginVerificationid;
+	String loginVerificationName;
+	String loginVerificationEmail;
+	ExtentTest test;
+	ExtentHtmlReporter htmlReporter;
+	ExtentReports extent;
+
+	@BeforeTest
+	public void beforeTest() {
+
+		htmlReporter = new ExtentHtmlReporter("TestReport.html");
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+
+		
+
+	}
+
+	@Test(priority = 1)
+	public void createUser() throws IOException {
+		
+		test = extent.createTest("Verify createUser api", "Checking the Title");
+		test.log(Status.INFO, "createUsertest started Successfully");
+		postContent = createBody();
+		
+		Response createUserResponse = given().spec(requestSpecification()).body(postContent).when().post("/authaccount/registration")
+				.then().spec(responseSpecification()).extract().response();
+		
+		
+		
+		test.log(Status.INFO,"Response Code : " + "\n" + createUserResponse.statusCode());
+		test.log(Status.INFO,"Response " + "\n" + createUserResponse.asString() );
+		 
+		
+		createUserId = createUserResponse.jsonPath().getString("data.Id");
+		createUserName = createUserResponse.jsonPath().getString("data.Name");
+		createUserEmail = createUserResponse.jsonPath().getString("data.Email");
+
+	}
+
+	@Test(priority = 2)
+	public void loginVerification() throws IOException {
+
+		test = extent.createTest("Verify loginVerification api", "Checking the Title");
+		test.log(Status.INFO, "loginVerificationrTest started Successfully");
+		postContent.remove("name");
+		Response loginVerificationResponse = given().spec(requestSpecification()).body(postContent).when().post("/authaccount/login").then()
+				.spec(responseSpecification()).log().all().extract().response();
+		
+
+		test.log(Status.INFO,"Response Code : " + "\n" + loginVerificationResponse.statusCode());
+		test.log(Status.INFO,"Response " + "\n" + loginVerificationResponse.asString() );
+		
+		assertEquals(createUserName, getJsonPath(loginVerificationResponse, "data.Name"));
+		assertEquals(createUserEmail, getJsonPath(loginVerificationResponse, "data.Email"));
+		assertEquals(createUserId, getJsonPath(loginVerificationResponse, "data.Id"));
+		
+
+		loginVerificationToken = loginVerificationResponse.jsonPath().getString("data.Token");
+		loginVerificationid = loginVerificationResponse.jsonPath().getString("data.Id");
+		loginVerificationName = loginVerificationResponse.jsonPath().getString("data.Name");
+		loginVerificationEmail = loginVerificationResponse.jsonPath().getString("data.Email");
+
+		
+		
+	}
+
+	@Test(priority = 3)
+	public void getUserById() throws IOException {
+
+		test = extent.createTest("Verify getUserById api", "Checking the Title");
+		test.log(Status.INFO, "getUserByIdTest started Successfully");
+	
+		Response getUserByIdResponse = given().spec(requestSpecification()).header("Authorization", "Bearer " + loginVerificationToken).when()
+				.get("/users/" + loginVerificationid).then().spec(responseSpecification()).log().all().extract().response();
+		
+		
+		test.log(Status.INFO,"Response Code : " + "\n" + getUserByIdResponse.statusCode());
+		test.log(Status.INFO,"Response " + "\n" + getUserByIdResponse.asString() );
+
+		
+		assertEquals(loginVerificationName, getJsonPath(getUserByIdResponse, "name"));
+		assertEquals(loginVerificationEmail, getJsonPath(getUserByIdResponse, "email"));
+		assertEquals(loginVerificationid, getJsonPath(getUserByIdResponse, "id"));
+
+		System.out.println("getUserById" + getUserByIdResponse.asString());
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+		extent.flush();
+	}
+
+	@AfterMethod
+	public void afterMethod(ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			test.log(Status.FAIL, "Test failed " + result.getThrowable());
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			test.log(Status.SKIP, "Test skipped " + result.getThrowable());
+		} else {
+			test.log(Status.PASS, "Test passed");
+		}
+
+	}
+}
